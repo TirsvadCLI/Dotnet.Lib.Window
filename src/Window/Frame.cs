9a80a7e9f753bd597dc701be5b140855bae9ca67
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-30 07:38:18 +0800

    Implement FrameBBase

--- src/Window/Frame.cs
+++ src/Window/Frame.cs
@@ -1,6 +1,15 @@
-namespace TirsvadCLI.Window
+﻿namespace TirsvadCLI.Window;
+using TirsvadCLI.Window.Base;
+using TirsvadCLI.Window.Model;
+
+public class Frame : FrameBase
 {
-    class Frame
+    public Frame(int width, int height) : base(width, height)
     {
+        WindowBuffer = new FrameBuffer(width, height);
+        CursorPosition = new Position(0, 0);
+        CursorStartPosition = new Position(0, 0);
     }
+    public Position CursorPosition { get; set; } ///> Actual cursor position
+    public Position CursorStartPosition { get; set; } ///> Start position of the cursor
 }
