94c99ca193107d0aeee8373b640b1539aeca6c3d
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-30 07:36:54 +0800

    Add Example code

---
+++ src/Example/Example.cs
@@ -0,0 +1,16 @@
+﻿namespace Example;
+using TirsvadCLI.Window;
+internal class Example
+{
+    Window window;
+
+    public Example()
+    {
+        window = new Window(80, 40); ///> Create a new window with 80 columns and 40 rows
+
+    }
+    static void Main(string[] args)
+    {
+
+    }
+}
